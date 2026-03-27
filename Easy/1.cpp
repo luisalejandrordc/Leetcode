73572feef7a753d503d7bdadade06dfe7f8a7e1a
Luis Alejandro <luisalejandrordc@gmail.com>
2026-03-27 02:11:03 -0500

    Update Easy/1.cpp

--- Easy/1.cpp
+++ Easy/1.cpp
@@ -27,21 +27,37 @@ using namespace std;
 // };
 
 // 10 ms
+// class Solution {
+// public:
+//   vector<int> twoSum(vector<int> &nums, int target) {
+//     unordered_map<int, int> map;
+//     for (int i = 0; i < nums.size(); i++) {
+//       map[nums[i]] = i;
+//     }
+//     for (int i = 0; i < nums.size(); i++) {
+//       int diff = target - nums[i];
+//       if (map.count(diff)) {
+//         int idx = map[diff];
+//         if (idx != i) {
+//           return {i, idx};
+//         }
+//       }
+//     }
+//     return {};
+//   }
+// };
+
+// 1 ms
 class Solution {
 public:
   vector<int> twoSum(vector<int> &nums, int target) {
     unordered_map<int, int> map;
     for (int i = 0; i < nums.size(); i++) {
-      map[nums[i]] = i;
-    }
-    for (int i = 0; i < nums.size(); i++) {
       int diff = target - nums[i];
       if (map.count(diff)) {
-        int idx = map[diff];
-        if (idx != i) {
-          return {i, idx};
-        }
+        return {i, map[diff]};
       }
+      map[nums[i]] = i;
     }
     return {};
   }
