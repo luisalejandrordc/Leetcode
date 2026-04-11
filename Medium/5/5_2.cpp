0177d482a1951e4db88f5a7c05ea1bd866412590
Luis Alejandro <luisalejandrordc@gmail.com>
2026-04-10 21:55:41 -0500

    Add Medium/5/5_2.cpp solution

--- Medium/5/5_2.cpp
+++ Medium/5/5_2.cpp
@@ -2,27 +2,40 @@
 
 using namespace std;
 
+string palindromeCenterIdx(const string &s, const int &idx) {
+  // Odd Palindrome
+  int k = 0;
+  while (idx - k >= 0 && idx + k < s.length() && s[idx - k] == s[idx + k])
+    k++;
+  int odd_len = 1 + 2 * (k - 1);
+
+  // Even Palindrome
+  int m = 0;
+  while (idx - m >= 0 && idx + 1 + m < s.length() &&
+         s[idx - m] == s[idx + 1 + m])
+    m++;
+  int even_len = 2 * m;
+
+  return odd_len > even_len ? s.substr(idx - (k - 1), odd_len)
+                            : s.substr(idx - (m - 1), even_len);
+}
+
 class Solution {
 public:
   string longestPalindrome(string s) {
     string ans;
-    for (int i = 0; i < s.length() - ans.length(); i++) {
-      for (int j = 1; j < s.length() - i + 1; j++) {
-        string temp1 = s.substr(i, j);
-        string temp2;
-        temp2.reserve(temp1.length());
-        for (int k = j - 1; k >= 0; k--)
-          temp2 += temp1[k];
-        if (temp1 == temp2 && ans.length() < j)
-          ans = temp1;
-      }
+    for (int i = 0; i < s.length() - floor(ans.length() / 2); i++) {
+      string temp = palindromeCenterIdx(s, i);
+      if (temp.length() > ans.length())
+        ans = temp;
     }
-    return ans.length() != 0 ? ans : s;
+    return ans;
   }
 };
 
 int main() {
   cout << "It's Showtime Folks!" << endl;
+  cout << "Solution: " << Solution().longestPalindrome("cabba") << endl;
   cout << "Solution: "
        << Solution().longestPalindrome(
               "zudfweormatjycujjirzjpyrmaxurectxrtqedmmgergwdvjmjtstdhcihacqnot"
