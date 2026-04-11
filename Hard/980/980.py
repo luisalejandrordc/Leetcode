fa2160828fe5fada02fa5b932a487bd13eef6227
Luis Alejandro <luisalejandrordc@gmail.com>
2026-04-11 14:39:25 -0500

    Add Hard/980/980.py solution

--- Hard/980/980.py
+++ Hard/980/980.py
@@ -7,5 +7,56 @@
 # Return the number of 4-directional walks from the starting square to the
 # ending square, that walk over every non-obstacle square exactly once.
 
+DIRECTIONS = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+OBSTACLE = -1
+EMPTY = 0
+START = 1
+END = 2
+
+
+def find_start(grid: list[list[int]], m: int, n: int) -> list[int]:
+    for i in range(m):
+        for j in range(n):
+            if grid[i][j] == START:
+                return [i, j]
+    return [0, 0]
+
+
+def find_empty_cells(grid: list[list[int]], m: int, n: int) -> int:
+    rem = 0
+    for i in range(m):
+        for j in range(n):
+            if grid[i][j] == 0:
+                rem += 1
+    return rem
+
+
+def dfs(
+    grid: list[list[int]], m: int, n: int, r: int, c: int, rem: int, cnt: list[int]
+):
+    for direc in DIRECTIONS:
+        i = r + direc[0]
+        j = c + direc[1]
+        if 0 <= i < m and 0 <= j < n:
+            if grid[i][j] == END and rem == 0:
+                cnt[0] += 1
+            elif grid[i][j] == EMPTY:
+                grid[i][j] = OBSTACLE
+                dfs(grid, m, n, i, j, rem - 1, cnt)
+                grid[i][j] = EMPTY
+
+
+class Solution:
+    def uniquePathsIII(self, grid: list[list[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        [r, c] = find_start(grid, m, n)
+        rem = find_empty_cells(grid, m, n)
+        cnt = [0]
+        dfs(grid, m, n, r, c, rem, cnt)
+        return cnt[0]
+
 
 print("It's Showtime Folks!")
+grid = [[1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 2, -1]]
+print(f"Solution: {Solution().uniquePathsIII(grid)}")
