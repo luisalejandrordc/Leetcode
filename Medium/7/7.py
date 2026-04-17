8b87641dcd88a41d7455182cecf5190a6b41e890
Luis Alejandro <luisalejandrordc@gmail.com>
2026-04-17 10:25:54 -0500

    Add Medium/7/7.py solution

---
+++ Medium/7/7.py
@@ -0,0 +1,31 @@
+# import math
+
+# INT_MIN = -math.pow(2, 31)
+# INT_MAX = -(INT_MIN + 1)
+INT_MIN = -2147483648
+INT_MAX = 2147483647
+
+
+# Implement C++ modulus operator
+def cpp_mod(a: int, b: int) -> int:
+    return a - b * int(a / b)
+
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        ans = 0
+        while x != 0:
+            digit = cpp_mod(x, 10)
+            if ans > INT_MAX / 10 or (ans == INT_MAX / 10 and digit > 7):
+                return 0
+            if ans < INT_MIN / 10 or (ans == INT_MIN / 10 and digit < -8):
+                return 0
+            ans = ans * 10 + digit
+            x = int(x / 10)
+        return ans
+
+
+print("It's Showtime Folks!")
+print(f"Solution: {Solution().reverse(123)}")
+print(f"Solution: {Solution().reverse(-12)}")
+print(f"Solution: {Solution().reverse(1534236469)}")
