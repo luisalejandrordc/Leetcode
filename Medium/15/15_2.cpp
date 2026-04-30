8f0ae2159ded631244ff7f599584e1f2885104f3
Luis Alejandro <luisalejandrordc@gmail.com>
2026-04-30 13:18:02 -0500

    Add Medium/15/15_2.cpp solution

--- Medium/15/15_2.cpp
+++ Medium/15/15_2.cpp
@@ -1,41 +1,25 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-void printSolution(const vector<vector<int>> &sol) {
-  cout << "Solution:" << endl;
-  for (vector<int> v : sol) {
-    cout << "[\t";
-    for (int x : v)
-      cout << x << "\t";
-    cout << "]" << endl;
+void printVector(vector<int> &nums) {
+  int len = nums.size();
+  cout << "[";
+  for (int i = 0; i < len; i++) {
+    cout << nums[i];
+    if (i != len - 1)
+      cout << "\t";
   }
+  cout << "]" << endl;
 }
 
-vector<vector<int>> getCombinations(vector<int> &nums, int k, int start = 0) {
-  vector<vector<int>> coms;
-  if (k == 1) {
-    for (int i = start; i < nums.size(); i++)
-      coms.push_back({nums[i]});
-    return coms;
-  }
-  for (int i = start; i <= nums.size() - k; i++) {
-    for (vector<int> com : getCombinations(nums, k - 1, i + 1)) {
-      com.insert(com.begin(), nums[i]);
-      coms.push_back(com);
-    }
-  }
-  return coms;
-}
-
-void orderArray(vector<int> &nums) {
+void sortVector(vector<int> &nums) {
   int i = 0;
   while (i < nums.size() - 1) {
     if (nums[i] > nums[i + 1]) {
-      int temp = nums[i];
-      nums[i] = nums[i + 1];
-      nums[i + 1] = temp;
+      swap(nums[i], nums[i + 1]);
       if (i > 0)
         i--;
     } else
@@ -43,29 +27,32 @@ void orderArray(vector<int> &nums) {
   }
 }
 
-bool alreadyExists(vector<vector<int>> &nums, vector<int> &com) {
-  for (vector<int> rec : nums) {
-    bool exists = true;
-    for (int i = 0; i < rec.size(); i++)
-      if (rec[i] != com[i])
-        exists = false;
-    if (exists)
-      return true;
-  }
-  return false;
-}
-
 class Solution {
 public:
   vector<vector<int>> threeSum(vector<int> &nums) {
     vector<vector<int>> ans;
-    orderArray(nums);
-    for (vector<int> com : getCombinations(nums, 3)) {
-      int sum = 0;
-      for (int num : com)
-        sum += num;
-      if (sum == 0 && !alreadyExists(ans, com))
-        ans.push_back(com);
+    int len = nums.size();
+    sort(nums.begin(), nums.end());
+    // sortVector(nums);
+    for (int i = 0; i < len - 2; i++) {
+      if (i > 0 && nums[i] == nums[i - 1])
+        continue;
+      int l = i + 1, r = len - 1;
+      while (l < r) {
+        int sum = nums[i] + nums[l] + nums[r];
+        if (sum == 0) {
+          ans.push_back({nums[i], nums[l], nums[r]});
+          while (l < r && nums[l] == nums[l + 1])
+            l++;
+          l++;
+          while (l < r && nums[r] == nums[r - 1])
+            r--;
+          r--;
+        } else if (sum < 0)
+          l++;
+        else
+          r--;
+      }
     }
     return ans;
   }
@@ -73,9 +60,20 @@ public:
 
 int main() {
   cout << "It's Showtime Folks!" << endl;
-  Solution s;
+  // Testcase 1
+  cout << "Testcase 1:" << endl;
   vector<int> nums = {-1, 0, 1, 2, -1, -4};
-  printSolution(s.threeSum(nums));
+  vector<vector<int>> ans = Solution().threeSum(nums);
+  for (vector<int> v : ans)
+    printVector(v);
+  // Testcase 2
+  cout << "Testcase 2:" << endl;
+  nums = {-1, 0, 1, 0};
+  ans = Solution().threeSum(nums);
+  for (vector<int> v : ans)
+    printVector(v);
+  // Testcase 3
+  cout << "Testcase 3:" << endl;
   nums = {-15, 10,  0,  -2,  14,  -1,  -10, -14, 10,  12,  6,   -6,  10,  2,
           -11, -9,  2,  13,  2,   -9,  -14, -12, -10, -12, 13,  13,  -10, -3,
           2,   -11, 3,  -6,  6,   10,  7,   5,   -13, 4,   -2,  12,  1,   -11,
@@ -84,6 +82,8 @@ int main() {
           -1,  -11, 4,  -10, -8,  -13, -15, 2,   -12, 8,   -2,  -12, -14, -10,
           -8,  6,   2,  -5,  -7,  -11, 7,   14,  -6,  -10, -12, 8,   -4,  -10,
           -5,  14,  -3, 9,   -12, 8,   14,  -13};
-  printSolution(s.threeSum(nums));
+  ans = Solution().threeSum(nums);
+  for (vector<int> v : ans)
+    printVector(v);
   return 0;
 }
