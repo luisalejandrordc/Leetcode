1a139718f458c1ee6569425bbd1da58a6d98d541
Luis Alejandro <luisalejandrordc@gmail.com>
2026-05-03 19:08:27 -0500

    Add Medium/46/46_2.ts solution

--- Medium/46/46_1.ts
+++ Medium/46/46_1.ts
@@ -1,20 +1,16 @@
 export {};
 
-function backtrack(nums: number[], a: number, result: number[][]) {
-  if (a === nums.length - 1) {
-    result.push([...nums]); // push a copy of nums
-    return;
-  }
-  for (let b = a; b < nums.length; b++) {
-    [nums[a], nums[b]] = [nums[b], nums[a]];
-    backtrack(nums, a + 1, result);
-    [nums[a], nums[b]] = [nums[b], nums[a]];
-  }
-}
-
 function permute(nums: number[]): number[][] {
+  if (nums.length === 0) return [[]];
   let result: number[][] = [];
-  backtrack(nums, 0, result);
+  for (let i = 0; i < nums.length; i++) {
+    const temp = nums.splice(i, 1)[0];
+    for (const v of permute(nums)) {
+      v.unshift(temp);
+      result.push(v);
+    }
+    nums.splice(i, 0, temp);
+  }
   return result;
 }
 
