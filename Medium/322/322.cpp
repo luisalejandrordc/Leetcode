3f41a5194ac8dd0a2bccd544330c25e908b1e38f
Luis Alejandro <luisalejandrordc@gmail.com>
2026-08-18 18:09:52 -0500

    add medium/322/322.cpp solution

---
+++ Medium/322/322.cpp
@@ -0,0 +1,104 @@
+#include "../../include/utils.h"
+#include <functional>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int recs(vector<int> &coins, int idx, int amount) {
+  int quantity = amount / coins[idx];
+  int remainder = amount - quantity * coins[idx];
+  if (remainder == 0)
+    return quantity;
+  if (idx == coins.size() - 1)
+    return -1;
+  int result = INT_MAX;
+  while (quantity >= 0) {
+    int counter = recs(coins, idx + 1, remainder);
+    if (counter != -1)
+      result = min(result, counter);
+    remainder += coins[idx];
+    quantity--;
+  }
+  return result == INT_MAX ? -1 : result;
+}
+
+int coinChange(vector<int> &coins, int amount) {
+  sort(coins.begin(), coins.end(), greater<>());
+  return recs(coins, 0, amount);
+}
+
+// This solution is technically correct, it scans all possible ways
+// to make up the given amount, but it takes too much time, because
+// of multiple repetitions of recursion states instead of saving
+// those previous results and resuing them
+int recs3(vector<int> &coins, int idx, int amount) {
+  int quantity = amount / coins[idx];
+  int remainder = amount - quantity * coins[idx];
+  if (remainder == 0)
+    return quantity;
+  if (idx == coins.size() - 1)
+    return -1;
+  int result = INT_MAX;
+  while (quantity >= 0) {
+    int counter = recs3(coins, idx + 1, remainder);
+    if (counter != -1)
+      result = min(result, counter);
+    remainder += coins[idx];
+    quantity--;
+  }
+  return result == INT_MAX ? -1 : result;
+}
+
+int coinChange3(vector<int> &coins, int amount) {
+  sort(coins.begin(), coins.end(), greater<>());
+  return recs3(coins, 0, amount);
+}
+
+// This greedy solution has a problem: using as many of the largest coin
+// as possible does not mean you'll get the most optimal result (minimum)
+// Example: coins = {4, 3, 1}, amount = 6 -> Expected result = 2
+int recs2(vector<int> &coins, int idx, int amount) {
+  int quantity = amount / coins[idx];
+  int remainder = amount - quantity * coins[idx];
+  if (remainder == 0)
+    return quantity;
+  if (idx == coins.size() - 1)
+    return -1;
+  while (quantity >= 0) {
+    int counter = recs2(coins, idx + 1, remainder);
+    if (counter != -1)
+      return counter + quantity;
+    remainder += coins[idx];
+    quantity--;
+  }
+  return -1;
+}
+
+int coinChange2(vector<int> &coins, int amount) {
+  sort(coins.begin(), coins.end(), greater<>());
+  return recs2(coins, 0, amount);
+}
+
+// This solution does not consider all possible ways to make up the amount
+// It is restricted by taking away the largest coins from amount without
+// considering that maybe taking less or even none of them could end up
+// making up the amount successfully
+int coinChange1(vector<int> &coins, int amount) {
+  sort(coins.begin(), coins.end(), greater<>());
+  int counter = 0;
+  for (const int coin : coins)
+    while (amount >= coin) {
+      amount -= coin;
+      counter++;
+    }
+  return amount == 0 ? counter : -1;
+}
+
+int main() {
+  printTitle("Coin Change");
+  vector<int> coins = {4, 3, 1};
+  int amount = 6;
+  cout << "Solution : " << coinChange(coins, amount) << endl;
+  return 0;
+}
