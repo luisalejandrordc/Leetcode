054d482b73517d9c163d71e531451ae123550b89
Luis Alejandro <luisalejandrordc@gmail.com>
2026-09-01 12:55:40 -0500

    update printVector so it works with nested vectors and optional type

--- include/utils.h
+++ include/utils.h
@@ -4,6 +4,7 @@
 #include <functional>
 #include <iostream>
 #include <string>
+#include <type_traits>
 #include <utility>
 #include <vector>
 
@@ -14,14 +15,39 @@ inline void printTitle(const std::string &title) {
   std::cout << dashes << std::endl;
 }
 
-template <typename T> inline void printVector(const std::vector<T> &vec) {
+// std::optional<T> condition
+template <typename T> struct is_optional : std::false_type {};
+template <typename T> struct is_optional<std::optional<T>> : std::true_type {};
+template <typename T>
+inline constexpr bool is_optional_v = is_optional<T>::value;
+
+// std::vector<T> condition
+template <typename T> struct is_vector : std::false_type {};
+template <typename T> struct is_vector<std::vector<T>> : std::true_type {};
+template <typename T> inline constexpr bool is_vector_v = is_vector<T>::value;
+
+template <typename T>
+inline void printVector(const std::vector<T> &vec,
+                        bool is_initial_call = true) {
   std::cout << "{";
-  for (int i = 0; i < vec.size(); i++) {
+  for (size_t i = 0; i < vec.size(); i++) {
     if (i > 0)
       std::cout << ", ";
-    std::cout << vec[i];
+    if constexpr (is_vector_v<T>)
+      printVector(vec[i], false);
+    else {
+      if constexpr (is_optional_v<T>) {
+        if (vec[i].has_value())
+          std::cout << *vec[i];
+        else
+          std::cout << "null";
+      } else
+        std::cout << vec[i];
+    }
   }
-  std::cout << "}" << std::endl;
+  std::cout << "}";
+  if (is_initial_call)
+    std::cout << std::endl;
 }
 
 template <typename Func, typename... Args>
